41cb491046d06a0fa811f9413a9f3711b0737ddb
Shamyal Mahmood <142646958+shamyalmahmood@users.noreply.github.com>
2024-12-01 01:17:23 +0500

    Update LoginApp.java
    Corrected SQL error

--- Assignment 2/src/LoginApp.java
+++ Assignment 2/src/LoginApp.java
@@ -59,7 +59,8 @@ public class LoginApp extends JFrame {
     private String authenticateUser(String email) {
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String query = "SELECT name FROM User WHERE Email = ?";
+            // Updated SQL Query
+            String query = "SELECT name FROM User WHERE email = ?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, email);
             ResultSet rs = stmt.executeQuery();
@@ -79,7 +80,6 @@ public class LoginApp extends JFrame {
         SwingUtilities.invokeLater(() -> {
             LoginApp loginApp = new LoginApp();
             loginApp.setVisible(true);
-            System.out.println("Hello, World!");
         });
     }
 }
